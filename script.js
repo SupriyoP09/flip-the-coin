7683e8bfd57f4079f02fa519948e12642b0b2776
Supriyo Pramanik <supriyopramanik.2006@gmail.com>
2025-09-27 20:16:37 +0530

    save work

---
+++ script.js
@@ -0,0 +1,26 @@
+const coin = document.getElementById('coin')
+const flipbutton = document.getElementById('flipbutton')
+const result = document.getElementById('flipbutton')
+const headsCount = document.getElementById('flipbutton')
+const tailsCount = document.getElementById('flipbutton')
+const historyList = document.getElementById('flipbutton')
+const clearButton = document.getElementById('flipbutton')
+
+
+let heads = 0
+let tails = 0
+
+let history = []
+let isFlipping = false
+
+function flipCoin() {
+    if (isFlipping)
+}
+
+function addToHistory(outcome) {
+
+}
+
+function clearHistory() {
+
+}
